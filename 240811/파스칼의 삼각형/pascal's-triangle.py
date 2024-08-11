6ba78b64beffcd9cad9c391516c9df1a3952b798
maengjj <114388488+maengjj@users.noreply.github.com>
2024-08-11 22:22:01 +0900

    Title: 파스칼의 삼각형, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/파스칼의 삼각형/pascal's-triangle.py
+++ 240811/파스칼의 삼각형/pascal's-triangle.py
@@ -1,14 +1,19 @@
-n = int(input())
+triangle = [
+    [0 for _ in range(15)]
+    for _ in range(15)
+]
 
-triangle = []
+n = int(input())
+	
+for i in range(n):
+	triangle[i][0] = 1
+	triangle[i][i] = 1
 
 for i in range(n):
-    row = [1] * (i + 1)
+	for j in range(1, i):
+		triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
-    for j in range(1, i):
-        row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-    
-    triangle.append(row)
-
-for row in triangle:
-    print(" ".join(map(str, row)))
+for i in range(n):
+	for j in range(i + 1):
+		print(triangle[i][j], end=" ")
+	print()
