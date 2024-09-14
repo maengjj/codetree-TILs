4f9a82ec80873870b449b5408e5a63cff5a3faef
maengjj <114388488+maengjj@users.noreply.github.com>
2024-09-15 00:26:54 +0900

    Title: 높은 수에서 낮은 수까지, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240915/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 240915/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,7 +1,15 @@
 a, b = map(int, input().split())
-c = b-a
 
-while c >= 0 :
-    print(b, end=' ')
-    b -= 1
-    c -= 1
+
+if b>a : 
+    c = b-a
+    while c >= 0 :
+        print(b, end=' ')
+        b -= 1
+        c -= 1
+else : 
+    c = a-b
+    while c >= 0 :
+        print(a, end=' ')
+        a -= 1
+        c -= 1
